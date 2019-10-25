61646e3899d86d09af0cc643f0c71009beab9129
tnsihy <tnsihy@foxmail.com>
2019-10-25 17:41:57 +0800

    too long

---
+++ myapp/src/pages/Store.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Store extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>Store</div>
+         );
+    }
+}
+ 
+export default Store;
